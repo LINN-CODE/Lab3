6521dc8da87aa0af53e786dff5ed42b97d1a7610
Linn <LINNENT.23@ichat.sp.edu.sg>
2024-10-28 10:03:29 +0800

    git iginore

--- Test_bmi.py
+++ Test_bmi.py
@@ -6,7 +6,7 @@ def test_bmi_normal_weight():
     result = 0
     input_height = 1.8
     input_weight = 60
-    test_result = bmi.calculate_bmi(input_height,input_weight)
+    test_result = bmi.calculate_bmi_2(input_height,input_weight)
     assert(result == test_result)
     
 
@@ -14,12 +14,12 @@ def test_bmi_over_weight():
     result = 1
     input_height = 1.8
     input_weight = 90
-    test_result = bmi.calculate_bmi(input_height,input_weight)
+    test_result = bmi.calculate_bmi_2(input_height,input_weight)
     assert(result == test_result)
 
 def test_bmi_under_weight():
     result = -1
     input_height = 1.8
     input_weight = 35
-    test_result = bmi.calculate_bmi(input_height,input_weight)
+    test_result = bmi.calculate_bmi_2(input_height,input_weight)
     assert(result == test_result)
